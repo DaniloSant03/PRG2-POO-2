b05a11115f76719c43babc0045479c1b7dccc624
DaniloSant03 <75862223+DaniloSant03@users.noreply.github.com>
2020-12-11 18:25:18 -0300

    Add files via upload

---
+++ Exercício 2/Exercício 2/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_2
+{
+    class Quadrado
+    {
+        #region Atributos
+        double lado;
+        double area;
+        #endregion
+
+        #region Construtores
+        public Quadrado()
+        {
+            this.lado = 0;
+            this.area = 0;
+        }
+
+        public Quadrado(double lado)
+        {
+            this.lado = lado;
+            this.area = 0;
+        }
+        #endregion
+
+        #region Setters e Getters
+        public void SetLado(double lado)
+        {
+            this.lado = lado;
+        }
+
+        public double GetLado()
+        {
+            return this.lado;
+        }
+
+        public double GetArea()
+        {
+            return this.area;
+        }
+
+        #endregion
+
+        #region Métodos funcionais
+        public double CalcularArea()
+        {
+            this.area = this.lado * this.lado;
+            return this.area;
+        }
+
+        public double CalcularArea(double lado)
+        {
+            this.area = lado * lado;
+            return this.area;
+
+        }
+        #endregion
+    }
+}
